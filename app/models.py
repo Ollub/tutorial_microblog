33bf6adef3d2c3a932a2c167b700d27f9841adb5
RBatonov <funnysunbunny@gmail.com>
2019-02-26 08:32:20 +0300

    15. App structure optimization

--- app/models.py
+++ app/models.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 from hashlib import md5
 from time import time
-
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import current_app
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
-from app import db, login, app
+from app import db, login
+
 
 
 followers = db.Table('followers',
@@ -61,12 +62,13 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+            current_app.config['SECRET_KEY'],
+            algorithm='HS256').decode('utf-8')
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config['SECRET_KEY'],
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
                             algorithms=['HS256'])['reset_password']
         except:
             return
@@ -74,6 +76,10 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -84,7 +90,3 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
-
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
--- app/translate.py
+++ app/translate.py
@@ -1,18 +1,17 @@
 # import json
+import json
 import requests
-
+from flask import current_app
 from flask_babel import _
-
-from app import app
 
 
 def translate(text, dest_language):
-    if 'YA_KEY' not in app.config or \
-            not app.config['YA_KEY']:
+    if 'YA_KEY' not in current_app.config or \
+            not current_app.config['YA_KEY']:
         return _('Error: the translation service is not configured.')
     r = requests.get(
         'https://translate.yandex.net/api/v1.5/tr.json/translate'
-        '?key={}&text={}&lang={}&options=1'.format(app.config['YA_KEY'], text, dest_language))
+        '?key={}&text={}&lang={}&options=1'.format(current_app.config['YA_KEY'], text, dest_language))
     if r.status_code != 200:
         return _('Error: the translation service failed.')
     return r.json()['text'][0]
