4503db92617ae571305fc2682503afddb3d5e5b1
RBatonov <funnysunbunny@gmail.com>
2019-02-28 09:07:16 +0300

    16. Elasticsearch

--- config.py
+++ config.py
@@ -22,3 +22,5 @@ class Config(object):
     #translation
     LANGUAGES = ['en', 'ru']
     YA_KEY = 'trnsl.1.1.20190224T072926Z.b67620fcaa39011a.670b6351a26efb3b442511116be06521d6ed0f15'
+    #search engine
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL') or 'http://localhost:9200'
